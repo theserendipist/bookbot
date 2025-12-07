6e68eca38de95079eafe11adf3d11c073fb8a9c1
Adam Takacs <adam@theserendipist.com>
2025-12-07 20:35:33 +0100

    add better report feature

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 def main():
     from stats import get_num_words, get_num_chars, sort_chars
 
-    frankenstein = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book = get_book_text(book_path)
     
-    num_words = get_num_words(frankenstein)
-    print(f"Found {num_words} total words")
-
-    num_chars = get_num_chars(frankenstein)
-    print(num_chars)
+    num_words = get_num_words(book)
+    num_chars = get_num_chars(book)
 
     sorted_num_chars = sort_chars(num_chars)
-    print(sorted_num_chars)
+    
+    print_report(book_path, num_words, sorted_num_chars)
 
 def get_book_text(path):
     with open(path) as b:
         book = b.read()
     
     return book
+
+def print_report(book_path, num_words, sorted_num_chars):
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {book_path}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------""")
+    for item in sorted_num_chars:
+        if item["char"].isalpha():
+            char = item["char"]
+            count = item["num"]
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 main()
