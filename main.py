ccd452ad7f76cc2bea938a98da7afd2f7074adc4
Adam Takacs <adam@theserendipist.com>
2025-12-07 19:09:17 +0100

    add word count feat

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
-    print(frankenstein)
+    num_words = get_num_words(frankenstein)
+    print(f"Found {num_words} total words")
 
 def get_book_text(path):
     with open(path) as b:
         book = b.read()
     
     return book
+
+def get_num_words(book):
+    words = book.split()
+    num_words = len(words)
+
+    return num_words
 
 main()
