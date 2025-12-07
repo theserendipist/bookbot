e63f19721c28d7b4c6a6ea8272ea391247f47251
Adam Takacs <adam@theserendipist.com>
2025-12-07 19:31:30 +0100

    add character count feature

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
-    from stats import get_num_words
-    
+    from stats import get_num_words, get_num_chars
+
     frankenstein = get_book_text("books/frankenstein.txt")
+    
     num_words = get_num_words(frankenstein)
     print(f"Found {num_words} total words")
+
+    num_chars = get_num_chars(frankenstein)
+    print(num_chars)
 
 def get_book_text(path):
     with open(path) as b:
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def get_num_words(book):
     num_words = len(words)
 
     return num_words
+
+def get_num_chars(book):
+    book = book.lower()
+    num_chars = {}
+    for char in book:
+        if char in num_chars:
+            num_chars[char] += 1
+        else:
+            num_chars[char] = 1
+
+    return num_chars
