1e18b4251c5b2d594ef491b929f3ebe2cc102883
Adam Takacs <adam@theserendipist.com>
2025-12-07 20:44:29 +0100

    add support for different book inputs

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def main():
+    import sys
     from stats import get_num_words, get_num_chars, sort_chars
 
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     book = get_book_text(book_path)
     
     num_words = get_num_words(book)
